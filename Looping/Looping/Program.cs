7b10af5fb402ddbadd97cb5809171382be41543e
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-04 00:33:22 +0530

    Add files via upload

---
+++ Looping/Looping/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Looping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int n=int.Parse(Console.ReadLine());
+            for(int i = 1; i <=10; i++)
+            {
+               Console.WriteLine(i+"*"+n+"="+(i*n));
+            }
+        }
+    }
+}
